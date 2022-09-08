9a6dd79dea3d53f7a8dcfadda569165aaf196491
HattaMaulana <mahatta.maulana@eratani.co.id>
2022-09-08 15:24:46 +0700

    Finish code

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import firebase_admin
 
 from firebase_admin import messaging
+
+firebase_admin.initialize_app()
+
+registration_token = input("Please input target device key token:")
+data_notification = messaging.Message(
+    token = registration_token,
+    notification=messaging.Notification(
+        'test-title', 
+        'test-body',
+    )
+)
+
+response = messaging.send(data_notification)
+
+print(response)
+print("--> Done")
